aa52d854d4ffa62c8817b99fcda1d28676463e4c
EthanJWright <ewright362@gmail.com>
2023-07-01 19:07:23 -0600

    return whole node array

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use regex::Regex;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
-use regex::Regex;
 
 #[derive(Debug, Clone)]
 struct Node {
@@ -11,7 +11,7 @@ struct Node {
     flags: Vec<String>, // New field to store flags
 }
 
-fn parse_log_file(file_path: &str) -> io::Result<Node> {
+fn parse_log_file(file_path: &str) -> io::Result<Vec<Node>> {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
 
@@ -41,8 +41,6 @@ fn parse_log_file(file_path: &str) -> io::Result<Node> {
 
             if let Some(parent_index) = current_node_index {
                 nodes[parent_index].children.push(new_node_index);
-            } else {
-                root_index = Some(new_node_index); // Assign root_index here
             }
 
             current_node_index = Some(new_node_index);
@@ -60,9 +58,7 @@ fn parse_log_file(file_path: &str) -> io::Result<Node> {
 
             if let Some(parent_index) = current_node_index {
                 nodes[parent_index].children.push(new_node_index);
-            } else {
-                root_index = Some(new_node_index); // Assign root_index here
-            }
+            } 
 
             current_node_index = Some(new_node_index);
             nodes.push(new_node);
@@ -91,9 +87,7 @@ fn parse_log_file(file_path: &str) -> io::Result<Node> {
 
             if let Some(parent_index) = current_node_index {
                 nodes[parent_index].children.push(new_node_index);
-            } else {
-                root_index = Some(new_node_index); // Assign root_index here
-            }
+            } 
 
             current_node_index = Some(new_node_index);
             nodes.push(new_node);
@@ -103,19 +97,13 @@ fn parse_log_file(file_path: &str) -> io::Result<Node> {
         }
     }
 
-    println!("Nodes: {:#?}", nodes);
-    if let Some(root_index) = root_index {
-        Ok(nodes[root_index].clone())
-    } else {
-        Err(io::Error::new(io::ErrorKind::Other, "No root node found"))
-    }
+    Ok(nodes.clone())
 }
 
 fn main() {
-    if let Ok(root) = parse_log_file("input/haiku.txt") {
-        println!("{:#?}", root);
+    if let Ok(nodes) = parse_log_file("input/haiku.txt") {
+        println!("{:#?}", nodes);
     } else {
         eprintln!("Error parsing log file");
     }
 }
-
